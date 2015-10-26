5ed96e91230deb083c88d9f8b7a0fb95ff239027
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2015-10-26 15:22:41 -0400

    Changed test code. Replaced while loop with semaphores for more efficient testing of image download code.

--- GooglyPuffTests/GooglyPuffTests.m
+++ GooglyPuffTests/GooglyPuffTests.m
@@ -45,18 +45,47 @@ const int64_t kDefaultTimeoutLengthInNanoSeconds = 10000000000; // 10 Seconds
 
 - (void)downloadImageURLWithString:(NSString *)URLString
 {
+    
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
     NSURL *url = [NSURL URLWithString:URLString];
-    __block BOOL isFinishedDownloading = NO;
     __unused Photo *photo = [[Photo alloc]
                              initwithURL:url
                              withCompletionBlock:^(UIImage *image, NSError *error) {
                                  if (error) {
                                      XCTFail(@"%@ failed. %@", URLString, error);
                                  }
-                                 isFinishedDownloading = YES;
+                                 //increment semaphore by 1
+                                 dispatch_semaphore_signal(semaphore);
                              }];
     
-    while (!isFinishedDownloading) {}
+    // this waits on the semaphore with a given timeout
+    // blocks the curent thread until the semaphore has been signalled
+    // A non-zero return code means the timeout has been reached
+    // In this case, test is failed because it is deemed that the network
+    // should not take more than 10 seconds to return
+    dispatch_time_t timeoutTime = dispatch_time(DISPATCH_TIME_NOW, kDefaultTimeoutLengthInNanoSeconds);
+    if (dispatch_semaphore_wait(semaphore, timeoutTime)) {
+        XCTFail(@"%@ timed out", URLString);
+        }
+
+    
+    
+    
+//    NSURL *url = [NSURL URLWithString:URLString];
+//    __block BOOL isFinishedDownloading = NO;
+//    __unused Photo *photo = [[Photo alloc]
+//                             initwithURL:url
+//                             withCompletionBlock:^(UIImage *image, NSError *error) {
+//                                 if (error) {
+//                                     XCTFail(@"%@ failed. %@", URLString, error);
+//                                 }
+//                                 isFinishedDownloading = YES;
+//                             }];
+//    
+//  //spinlock: wasting valuable CPU cycles waiting in the while loop
+//    // Debug navigaor shows 100% + for CPU vs ~50% for above code
+//    while (!isFinishedDownloading) {}
 }
+
 
 @end
